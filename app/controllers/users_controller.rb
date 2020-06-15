17ad5b85a1c0bd3f0756840feffb36768a1ce1cc
marykang09 <mary.kang09@gmail.com>
2020-06-15 13:07:50 -0400

    auth and new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :first_name, :last_name)
+        params.require(:user).permit(:first_name, :last_name, :username)
     end
 
 end
